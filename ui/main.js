40b883233faf93681384b8ccf4dd4054d611b2bc
Abhijith Nair <abhijitnair6@gmail.com>
2017-08-15 23:06:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
 console.log('Loaded!');
+//Change the text of the main-text div
+var element = document.getElementby('main-text');
+
+element.innerHTML = "This is JavaScript";
+
+//Move the image
+
+var img = document.getElemntById('dina');
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 10;
+ img.style.marginLeft = marginRight + 'px';
+}
+
+
+img.onclick = function() {
+     img.style.marginLeft = '100px';
+     var interval = setinterval(moveRight,100)
+   
+    
+};
