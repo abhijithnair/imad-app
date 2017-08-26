618e4c3ac7d91236877a590f8aa2b46cbc87b958
Abhijith Nair <abhijitnair6@gmail.com>
2017-08-26 11:10:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,6 +128,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+    var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:pageName',function(req, res){
     // pageName == pageOne
     // pages[pageName] == {} content object for pageOne
