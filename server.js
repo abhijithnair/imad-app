f98218dc48d655debfec43b8b677120170156c4b
Abhijith Nair <abhijitnair6@gmail.com>
2017-08-26 16:34:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/', function (req, res) {
 });
 
 var pool= new Pool(config);
-app.get('test-db',function(req, res){
+app.get('/test-db',function(req, res){
     //Select a request and make a response
     //return a response
     pool.query('SELECT * FROM test',function(err,result){
