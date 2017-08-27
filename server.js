a293857129cf1d92cef247e42c03a3f32668cb0b
Abhijith Nair <abhijitnair6@gmail.com>
2017-08-27 19:47:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,8 +142,10 @@ app.get('/hash/:input',function(req, res){
   res.send(hashedString);
 });
 
-app.get('/create-user',function(req, res){
+app.post('/create-user',function(req, res){
    // username,password
+   var username = req.body.username;
+   var password = req.body.password;
    var salt = crypto.getRandomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "users"(username,password)VALUES($1, $2)',[username, dbString],function(err,result){
