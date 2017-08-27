48d9fe735cae57ac4b1c5ff42fa425397b906393
Abhijith Nair <abhijitnair6@gmail.com>
2017-08-27 19:49:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'abhijitnair6',
@@ -14,7 +15,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 var articles = {
     page1 : {
